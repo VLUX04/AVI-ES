725c0d7ea590f39a1bef3098e3ccf639b56dd7e7
Granja5 <ze.diogo.granja@gmail.com>
2023-11-29 09:42:42 +0000

    Airport Class

---
+++ Classes/Airport.cpp
@@ -0,0 +1,9 @@
+#include "Airport.h"
+
+Airport::Airport(std::string airportCode, std::string airportName, std::string city, std::string country, double latitude, double longitude) {airportCode_=airportCode; airportName_=airportName; city_=city; country_=country; latitude_=latitude; longitude_=longitude;}
+string Airport::get_AirportCode() {return airportCode_;}
+string Airport::get_AirportName() {return airportName_;}
+string Airport::get_City() {return city_;}
+string Airport::get_Country() {return country_;}
+double Airport::get_Latitude() {return latitude_;}
+double Airport::get_Longitude() {return longitude_;}
---
+++ Classes/Airport.h
@@ -0,0 +1,27 @@
+#ifndef PROJETO2AED_AIRPORT_H
+#define PROJETO2AED_AIRPORT_H
+
+#include <string>
+using namespace std;
+
+class Airport {
+private:
+    string airportCode_;
+    string airportName_;
+    string city_;
+    string country_;
+    double latitude_;
+    double longitude_;
+
+public:
+    Airport(string airportCode, string airportName, string city, string country, double latitude, double longitude);
+    string get_AirportCode();
+    string get_AirportName();
+    string get_City();
+    string get_Country();
+    double get_Latitude();
+    double get_Longitude();
+};
+
+
+#endif //PROJETO2AED_AIRPORT_H
