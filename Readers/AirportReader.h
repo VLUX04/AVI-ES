e14d7c9dffa81f1afb54b97357c5057476a44edd
VLUX04 <leonardo13122004@gmail.com>
2023-11-29 18:18:16 +0000

    Created Readers

---
+++ Readers/AirportReader.h
@@ -0,0 +1,8 @@
+//
+// Created by psclr on 29/11/2023.
+//
+
+#ifndef AVIÕES_AIRPORTREADER_H
+#define AVIÕES_AIRPORTREADER_H
+
+#endif //AVIÕES_AIRPORTREADER_H
---
+++ Readers/FlightsReader.h
@@ -0,0 +1,8 @@
+//
+// Created by psclr on 29/11/2023.
+//
+
+#ifndef AVIÕES_FLIGHTSREADER_H
+#define AVIÕES_FLIGHTSREADER_H
+
+#endif //AVIÕES_FLIGHTSREADER_H
